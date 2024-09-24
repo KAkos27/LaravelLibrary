a323117a228bc9925cdbd4d64674b938d6748564
KAkos27 <akoskosztolanyi@gmail.com>
2024-09-24 14:29:43 +0200

    pereg

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+
+Route::post('/user', [UserController::class, 'store']);
