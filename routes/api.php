bb16858a475cdb6d895af2da07c42bbfa2447468
KAkos27 <akoskosztolanyi@gmail.com>
2024-10-01 15:47:21 +0200

    eskesz

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CopyController;
+use App\Http\Controllers\LendingController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//Users routes
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/user/{id}', [UserController::class, 'show']);
 
@@ -20,7 +22,7 @@ Route::put('/user/{id}', [UserController::class, 'update']);
 Route::delete('/user/{id}', [UserController::class, 'destroy']);
 
 
-
+//Books routes
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/book/{id}', [BookController::class, 'show']);
 
@@ -31,7 +33,7 @@ Route::put('/book/{id}', [BookController::class, 'update']);
 Route::delete('/book/{id}', [BookController::class, 'destroy']);
 
 
-
+//Copies routes
 Route::get('/copies', [CopyController::class, 'index']);
 Route::get('/copy/{id}', [CopyController::class, 'show']);
 
@@ -40,3 +42,14 @@ Route::post('/copy', [CopyController::class, 'store']);
 Route::put('/copy/{id}', [CopyController::class, 'update']);
 
 Route::delete('/copy/{id}', [CopyController::class, 'destroy']);
+
+
+//Lendings routes
+Route::get('/lendings', [LendingController::class, 'index']);
+Route::get('/lending/{user_id}/{copy_id}/{start}', [LendingController::class, 'show']);
+
+Route::post('/lending', [LendingController::class, 'store']);
+
+Route::put('/lending/{user_id}/{copy_id}/{start}', [LendingController::class, 'update']);
+
+Route::delete('/lending/{user_id}/{copy_id}/{start}', [LendingController::class, 'destroy']);
